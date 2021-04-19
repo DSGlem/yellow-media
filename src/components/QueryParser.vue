e8246e0e93eb2e236b99295a3629d9141f1260a9
DSGlem <davidsporysh@gmail.com>
2021-04-20 00:51:13 +0300

    task 3: QueryParser

---
+++ src/components/QueryParser.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ queryObject }}</div>
+</template>
+<script>
+import { parseURLSearch } from "@/helpers";
+export default {
+  data() {
+    return {
+      queryObject: null,
+    };
+  },
+  created() {
+    const search = window.location.search;
+    this.queryObject = parseURLSearch(search);
+  },
+};
+</script>
