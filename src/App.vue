9b5f2f2f3fc356b6c57f980e74f099d2a2f7e61f
DSGlem <davidsporysh@gmail.com>
2021-04-20 00:53:43 +0300

    Demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <button @click="currentTab = 'FrequencyCharacter'">
+        FrequencyCharacter
+      </button>
+      <button @click="currentTab = 'TestForm'">TestForm</button>
+      <button @click="currentTab = 'QueryParser'">QueryParser</button>
+      <button @click="currentTab = 'ProductGallery'">ProductGallery</button>
+    </div>
+    <keep-alive>
+      <component :is="currentTab"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FrequencyCharacter from "@/components/FrequencyCharacter.vue";
+import TestForm from "@/components/TestForm.vue";
+import QueryParser from "@/components/QueryParser.vue";
+import ProductGallery from "@/components/ProductGallery/ProductGallery.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FrequencyCharacter,
+    TestForm,
+    QueryParser,
+    ProductGallery,
+  },
+  data() {
+    return {
+      currentTab: "FrequencyCharacter",
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap");
+@import "@/assets/styles/variables.scss";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin-top: 60px;
+
+  color: var(--color-font-main);
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
